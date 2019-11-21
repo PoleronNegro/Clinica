db4f40a4a822a7765691609dc5cf53c0f7612d74
PoleronNegro <os.torress@alumnos.duoc.cl>
2019-11-21 12:22:33 -0300

    cambios

--- Personas/urls.py
+++ Personas/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('aacounts/', include('django.contrib.auth.ulrs')),
+    path('acounts/', include('django.contrib.auth.ulrs')),
 ]
