a45c6f66f7aa57f4a904bf8f1384696717e5c989
sebastiancorleone <sebastian199615.ss@gmail.com>
2019-11-21 23:12:15 -0300

    vistas en proceso

--- Personas/urls.py
+++ Personas/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
     path('acounts/', include('django.contrib.auth.ulrs')),
+    path('medico/',views.medico,name='inicio')
 ]
--- Personas/views.py
+++ Personas/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponde
+from . import models
 # Create your views here.
+
+
+
+def medico(request):
+    return HttpResponde('medico')
